b67e3a41df782c38bea2b58eb9d2034d5c3d9cfc
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-03-14 11:04:58 +0100

    -HomeScreen cleanup

--- app/src/main/java/com/larsson/voicenote_android/features/HomeScreen.kt
+++ app/src/main/java/com/larsson/voicenote_android/features/HomeScreen.kt
@@ -2,17 +2,13 @@ package com.larsson.voicenote_android.features // ktlint-disable package-name
 
 import android.content.res.Configuration
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.larsson.voicenote_android.data.getUUID
 import com.larsson.voicenote_android.ui.NotesList
 import com.larsson.voicenote_android.ui.components.BottomBox
@@ -35,34 +31,28 @@ fun HomeScreen(
     val topToggleBar = notesViewModel.topToggleBarVisible
 
     Column(
-        modifier = modifier.background(MaterialTheme.colorScheme.background)
+        modifier = Modifier.background(MaterialTheme.colorScheme.background)
     ) {
-        Column(
-            verticalArrangement = Arrangement.SpaceBetween,
+        if (topToggleBar) {
+            TopToggleBar()
+        }
+        Box(
             modifier = Modifier
+                .weight(1f)
         ) {
-            Spacer(modifier = Modifier.height(30.dp))
-            if (topToggleBar) {
-                TopToggleBar()
+            if (newNoteVisible) {
+                NewNoteScreen(notesViewModel, newNoteId)
             }
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-            ) {
-                if (newNoteVisible) {
-                    NewNoteScreen(notesViewModel, newNoteId)
-                }
-                if (notesListVisible) {
-                    NotesList()
-                }
+            if (notesListVisible) {
+                NotesList()
             }
-            if (bottomBoxVisible) {
-                BottomBox(
-                    variant = Variant.NEW_NOTE_RECORD,
-                    onClickRight = {},
-                    onClickLeft = {}
-                )
-            }
+        }
+        if (bottomBoxVisible) {
+            BottomBox(
+                variant = Variant.NEW_NOTE_RECORD,
+                onClickRight = {},
+                onClickLeft = {}
+            )
         }
     }
 }
