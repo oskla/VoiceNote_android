8b462186871445d4790ac8d86dbbe553b7c61ed3
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-03-16 17:45:35 +0100

    Updated UI in bottomSheet

--- app/src/main/java/com/larsson/voicenote_android/ui/components/BottomSheet.kt
+++ app/src/main/java/com/larsson/voicenote_android/ui/components/BottomSheet.kt
@@ -1,16 +1,28 @@
 package com.larsson.voicenote_android.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Icon
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.SheetState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.larsson.voicenote_android.R
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -19,26 +31,42 @@ import kotlinx.coroutines.launch
 fun BottomSheet(
     openBottomSheet: MutableState<Boolean>,
     bottomSheetState: SheetState,
-    scope: CoroutineScope
+    scope: CoroutineScope,
+    modifier: Modifier = Modifier
 ) {
     if (openBottomSheet.value) {
         ModalBottomSheet(
+            containerColor = MaterialTheme.colorScheme.background,
             onDismissRequest = { openBottomSheet.value = false },
             sheetState = bottomSheetState
         ) {
-            Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                Button(
-                    // Note: If you provide logic outside of onDismissRequest to remove the sheet,
-                    // you must additionally handle intended state cleanup, if any.
-                    onClick = {
-                        scope.launch { bottomSheetState.hide() }.invokeOnCompletion {
-                            if (!bottomSheetState.isVisible) {
-                                openBottomSheet.value = false
+            Row(Modifier.fillMaxWidth().background(MaterialTheme.colorScheme.background), horizontalArrangement = Arrangement.Center) {
+                // Note: If you provide logic outside of onDismissRequest to remove the sheet,
+                // you must additionally handle intended state cleanup, if any.
+
+                Column(
+                    modifier = Modifier.padding(vertical = 40.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    IconButton(
+                        modifier = Modifier.size(71.dp),
+                        onClick = {
+                            scope.launch { bottomSheetState.hide() }.invokeOnCompletion {
+                                if (!bottomSheetState.isVisible) {
+                                    openBottomSheet.value = false
+                                }
                             }
                         }
+                    ) {
+                        Icon(
+                            painterResource(R.drawable.stop_icon),
+                            "stop recording",
+                            tint = MaterialTheme.colorScheme.onBackground
+                        )
                     }
-                ) {
-                    Text("Hide Bottom Sheet")
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text("Recording...")
                 }
             }
         }
