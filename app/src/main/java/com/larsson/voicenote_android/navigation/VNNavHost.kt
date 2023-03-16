f4b761f73849072bf494bca071095575cd453390
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-03-16 18:16:49 +0100

    Navgraph updated with viewmodel and bottomSheet

--- app/src/main/java/com/larsson/voicenote_android/navigation/VNNavHost.kt
+++ app/src/main/java/com/larsson/voicenote_android/navigation/VNNavHost.kt
@@ -1,23 +1,30 @@
 
 package com.larsson.voicenote_android.navigation
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.larsson.voicenote_android.di.viewModel
 import com.larsson.voicenote_android.features.HomeScreen
 import com.larsson.voicenote_android.features.NewNoteScreen
 import com.larsson.voicenote_android.ui.EditNoteScreen
 import com.larsson.voicenote_android.viewmodels.NotesViewModel
 import org.koin.androidx.compose.get
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NavGraph(navController: NavHostController = rememberNavController()) {
     val notesViewModel = remember { NotesViewModel() }
+    val openBottomSheet = rememberSaveable { mutableStateOf(false) }
+    val bottomSheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+    val scope = rememberCoroutineScope()
 
     NavHost(
         navController = navController,
@@ -26,12 +33,15 @@ fun NavGraph(navController: NavHostController = rememberNavController()) {
         composable(Screen.Home.route) {
             HomeScreen(
                 notesViewModel = notesViewModel,
-                navController = navController
+                navController = navController,
+                openBottomSheet = openBottomSheet,
+                bottomSheetState = bottomSheetState,
+                scope = scope
             )
         }
         composable(Screen.NewNote.route) {
-            NewNoteScreen(notesViewModel = notesViewModel, id = "", navController = navController)
+            NewNoteScreen(notesViewModel = notesViewModel, navController = navController)
         }
-        composable(Screen.EditNote.route) { EditNoteScreen(viewModel = get(), navController = navController) }
+        composable(Screen.EditNote.route) { EditNoteScreen(viewModel = notesViewModel, navController = navController) }
     }
 }
