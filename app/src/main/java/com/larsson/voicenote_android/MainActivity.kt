813e6380e4620b12281374d10068474337296051
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-03-16 18:19:31 +0100

    imports

--- app/src/main/java/com/larsson/voicenote_android/MainActivity.kt
+++ app/src/main/java/com/larsson/voicenote_android/MainActivity.kt
@@ -3,8 +3,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
