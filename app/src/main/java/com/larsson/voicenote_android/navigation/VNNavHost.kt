00516b8241426af4a24014f09d04f3ebbaad200b
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-03-16 13:18:38 +0100

    Added viewmodel instance so it would survive navigation

--- app/src/main/java/com/larsson/voicenote_android/navigation/VNNavHost.kt
+++ app/src/main/java/com/larsson/voicenote_android/navigation/VNNavHost.kt
@@ -2,19 +2,36 @@
 package com.larsson.voicenote_android.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.larsson.voicenote_android.di.viewModel
 import com.larsson.voicenote_android.features.HomeScreen
+import com.larsson.voicenote_android.features.NewNoteScreen
+import com.larsson.voicenote_android.ui.EditNoteScreen
+import com.larsson.voicenote_android.viewmodels.NotesViewModel
 import org.koin.androidx.compose.get
 
-
 @Composable
-fun NavGraph(navController: NavHostController) {
+fun NavGraph(navController: NavHostController = rememberNavController()) {
+    val notesViewModel = remember { NotesViewModel() }
+
     NavHost(
         navController = navController,
         startDestination = Screen.Home.route
     ) {
-        composable(Screen.NewNote.route) { HomeScreen(notesViewModel = get()) }
+        composable(Screen.Home.route) {
+            HomeScreen(
+                notesViewModel = notesViewModel,
+                navController = navController
+            )
+        }
+        composable(Screen.NewNote.route) {
+            NewNoteScreen(notesViewModel = notesViewModel, id = "", navController = navController)
+        }
+        composable(Screen.EditNote.route) { EditNoteScreen(viewModel = get(), navController = navController) }
     }
 }
