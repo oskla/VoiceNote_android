2351996a0fc370f3be989a4d9fd5fac6ef664361
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-03-16 18:16:14 +0100

    Functionality for editnote working

--- app/src/main/java/com/larsson/voicenote_android/viewmodels/NotesViewModel.kt
+++ app/src/main/java/com/larsson/voicenote_android/viewmodels/NotesViewModel.kt
@@ -1,11 +1,12 @@
 package com.larsson.voicenote_android.viewmodels
 
 import android.util.Log
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import com.larsson.voicenote_android.data.*
-import org.koin.androidx.compose.get
-import java.util.*
+import com.larsson.voicenote_android.data.Note
 
 class NotesViewModel : ViewModel() {
 
@@ -15,10 +16,11 @@ class NotesViewModel : ViewModel() {
     var recordingsListVisible by mutableStateOf(false)
     var bottomBoxVisible by mutableStateOf(true)
     var topToggleBarVisible by mutableStateOf(true)
+    private var selectedNoteId by mutableStateOf("")
 
     private var _notes = mutableStateListOf<Note>(
         Note("3", "Hej", "asg"),
-        Note("3", "Hsfej", "fsasg")
+        Note("2", "Hsfej", "fsasg")
     )
     val notes: List<Note> = _notes
 
@@ -32,8 +34,16 @@ class NotesViewModel : ViewModel() {
         )
     }
 
+    fun selectNoteById(id: String) {
+        selectedNoteId = notes.first { it.id == id }.id
+    }
+
+    fun getSelectedNote(): Note {
+        return getNoteById(selectedNoteId)
+    }
+
     fun getNoteById(id: String): Note {
-        return notes.first { it.id.toString() == id }
+        return notes.first { it.id == id }
     }
 
     fun getAllNotes(): List<Note> {
