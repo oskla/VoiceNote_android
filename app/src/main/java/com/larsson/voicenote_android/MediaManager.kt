c78ff37b0b55339fc502a632a5d1770cfedd6ab6
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-05-06 11:57:44 +0200

    - improve functionality

--- app/src/main/java/com/larsson/voicenote_android/MediaManager.kt
+++ app/src/main/java/com/larsson/voicenote_android/MediaManager.kt
@@ -2,6 +2,7 @@ package com.larsson.voicenote_android
 
 import android.content.Context
 import android.media.MediaPlayer
+import android.util.Log
 import java.io.File
 import java.io.IOException
 
@@ -9,21 +10,38 @@ class MediaManager(private val context: Context) {
 
     private var mediaPlayer: MediaPlayer? = null
     private var onPreparedListener: (() -> Unit)? = null
+    private var currentPath: String? = null // TODO remove this?
+    private var currentRecordingId: String? = null
 
-    fun start(path: String) {
+    fun start(recordingId: String) {
         if (mediaPlayer == null) {
             mediaPlayer = MediaPlayer()
         }
 
         try {
+            val file = File(context.cacheDir, recordingId).absolutePath.apply { currentPath = this }
+            Log.d("audioMediaManager", "currentPath: $currentPath")
+            Log.d("audioMediaManager", "recordingID: $recordingId")
             mediaPlayer?.reset()
-            mediaPlayer?.setDataSource(File(context.cacheDir, path).absolutePath)
+            mediaPlayer?.setDataSource(file)
             mediaPlayer?.prepare()
         } catch (e: IOException) {
             e.printStackTrace()
         }
 
         mediaPlayer?.start()
+    }
+
+    fun resume() {
+        mediaPlayer?.start()
+    }
+
+    fun getCurrentPath(): String? { // TODO remove this?
+        return currentPath
+    }
+
+    fun getCurrentRecordingId(): String? {
+        return currentRecordingId
     }
 
     fun stop() {
@@ -34,5 +52,13 @@ class MediaManager(private val context: Context) {
 
     fun pause() {
         mediaPlayer?.pause()
+    }
+
+    fun getCurrentPosition(): Int? {
+        return mediaPlayer?.currentPosition
+    }
+
+    fun isPlaying(): Boolean? {
+        return mediaPlayer?.isPlaying
     }
 }
