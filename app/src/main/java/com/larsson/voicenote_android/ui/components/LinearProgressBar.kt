ecb55c86b578b752b4feb6263f83bd5a69501a83
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-05-15 13:26:28 +0200

    - incorporated with actual data

--- app/src/main/java/com/larsson/voicenote_android/ui/components/LinearProgressBar.kt
+++ app/src/main/java/com/larsson/voicenote_android/ui/components/LinearProgressBar.kt
@@ -6,27 +6,42 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.SliderDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 
-// TODO - hook this up to actual audio
 @Composable
 fun LinearProgressBar(
     modifier: Modifier = Modifier,
-    progress: Float,
+    currentPosition: Int,
     color: Color,
     backgroundColor: Color = MaterialTheme.colorScheme.secondary,
-    onProgressChanged: (Float) -> Unit,
+    seekTo: (Float) -> Unit,
+    durationFloat: Float,
 ) {
+    val sliderPosition = remember { mutableStateOf(currentPosition.toFloat()) }
+
+    SideEffect {
+        if (currentPosition.toFloat() != sliderPosition.value) {
+            sliderPosition.value = currentPosition.toFloat()
+        }
+    }
+
     Box(
         modifier = modifier.background(backgroundColor),
     ) {
         Slider(
             modifier = Modifier,
-            value = progress,
-            onValueChange = { onProgressChanged.invoke(it) },
+            value = sliderPosition.value,
+            onValueChange = { newValue ->
+                sliderPosition.value = newValue
+                seekTo.invoke(newValue)
+            },
             colors = SliderDefaults.colors(thumbColor = color, activeTrackColor = color, inactiveTrackColor = color.copy(0.1f)),
-            valueRange = 0f..1f,
+            valueRange = 0f..durationFloat,
+            onValueChangeFinished = { },
         )
     }
 }
