fd86599c96711918aa12f8164714264b0ef5ef4c
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-05-20 12:54:27 +0200

    - reuse component instead of making almost the same twice

--- app/src/main/java/com/larsson/voicenote_android/ui/components/RecordingMenu.kt
+++ app/src/main/java/com/larsson/voicenote_android/ui/components/RecordingMenu.kt
@@ -6,18 +6,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.larsson.voicenote_android.PlayerState
 import com.larsson.voicenote_android.data.entity.RecordingEntity
 import com.larsson.voicenote_android.ui.theme.VoiceNote_androidTheme
@@ -25,14 +18,15 @@ import com.larsson.voicenote_android.ui.theme.VoiceNote_androidTheme
 @Composable
 fun RecordingMenu(
     modifier: Modifier = Modifier,
-    noteId: String,
     recordings: List<RecordingEntity>,
     onClickPlay: (String) -> Unit,
     onClickPause: () -> Unit,
     playerState: PlayerState,
-) {
-    var selectedRecordingId by remember { mutableStateOf<String?>(null) }
+    onClickContainer: () -> Unit,
+    currentPosition: Int,
+    seekTo: (Float) -> Unit,
 
+) {
     LazyColumn(
         modifier = modifier
             .fillMaxWidth()
@@ -42,12 +36,12 @@ fun RecordingMenu(
     ) {
         if (recordings.isEmpty()) {
             item {
-                RecordingMenuItem(
+                RecordingMenuItem( // TODO Clean this up, maybe cleanest to either make a separate component for this, or make a different state: "isEmpty".
                     title = "You have no recordings yet",
                     date = "",
                     durationText = "",
                     id = "",
-                    progress = 60, // TODO should come from viewmodel,
+                    progress = 0, // TODO should come from viewmodel,
                     isSelected = false,
                     onClickContainer = {
                     },
@@ -59,27 +53,18 @@ fun RecordingMenu(
                 )
             }
         }
-        itemsIndexed(recordings) { index, recording ->
-            val isSelected = (recording.recordingId == selectedRecordingId) // Checks what recording is actually pressed.
-            RecordingMenuItem(
-                title = recording.recordingTitle,
-                date = recording.recordingDate,
-                durationText = recording.recordingDuration,
-                id = recording.recordingId,
-                progress = 60, // TODO should come from viewmodel,
-                isSelected = isSelected,
-                onClickContainer = {
-                    selectedRecordingId = if (isSelected) null else recording.recordingId
-                },
-                isFirstItem = index < 1,
-                onClickPlay = { onClickPlay(recording.recordingTitle) },
-                onClickPause = onClickPause,
-                isPlaying = false, // TODO should come from viewmodel
-                seekTo = {}, // TODO insert from viewmodel
-
-            )
-            Divider(color = Color.Transparent, thickness = 2.dp)
-        }
+    }
+    if (recordings.isNotEmpty()) {
+        RecordingsList(
+            recordings = recordings,
+            onClickPlay = onClickPlay,
+            onClickPause = onClickPause,
+            playerState = playerState,
+            onClickContainer = onClickContainer,
+            currentPosition = currentPosition,
+            seekTo = seekTo,
+            isMenu = true,
+        )
     }
 }
 
@@ -93,6 +78,14 @@ private const val componentName = "Recording Menu Item Player"
 @Composable
 fun RecordingMenuPreview() {
     VoiceNote_androidTheme {
-        RecordingMenu(noteId = "2", recordings = listOf(), onClickPlay = {}, onClickPause = {}, playerState = PlayerState.Paused)
+        RecordingMenu(
+            recordings = listOf(),
+            onClickPlay = {},
+            onClickPause = {},
+            playerState = PlayerState.Paused,
+            onClickContainer = {},
+            seekTo = {},
+            currentPosition = 0,
+        )
     }
 }
