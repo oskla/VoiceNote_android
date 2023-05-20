4df5c7dbd011852e29d1aa8b29ab61e340512136
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2023-05-20 12:53:45 +0200

    - break out component

--- app/src/main/java/com/larsson/voicenote_android/ui/components/RecordingMenuItem.kt
+++ app/src/main/java/com/larsson/voicenote_android/ui/components/RecordingMenuItem.kt
@@ -4,91 +4,22 @@ import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.Text
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.larsson.voicenote_android.helpers.TimeFormatter
 import com.larsson.voicenote_android.helpers.dateFormatter
-import com.larsson.voicenote_android.ui.theme.SpaceGroteskFontFamily
 import com.larsson.voicenote_android.ui.theme.VoiceNote_androidTheme
-
-@Composable
-fun RecordingMenuItemBase(
-    title: String,
-    date: String,
-    id: String,
-    duration: String,
-    color: Color = MaterialTheme.colorScheme.secondary,
-    isFirstItem: Boolean,
-
-) {
-    val roundedCornerShape = RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp, bottomStart = 0.dp, bottomEnd = 0.dp)
-    Card(
-        modifier = Modifier.wrapContentHeight(),
-        backgroundColor = MaterialTheme.colorScheme.background,
-        elevation = 0.dp,
-        shape = if (isFirstItem) roundedCornerShape else RectangleShape,
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 16.dp, vertical = 12.dp),
-        ) {
-            Text(
-                text = title,
-                color = MaterialTheme.colorScheme.onBackground,
-                fontSize = 14.sp,
-                fontFamily = SpaceGroteskFontFamily,
-                fontWeight = FontWeight.W700,
-            )
-            Spacer(modifier = Modifier.height(6.dp))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-            ) {
-                Text(
-                    text = date,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    fontFamily = SpaceGroteskFontFamily,
-                    fontWeight = FontWeight.Thin,
-                    style = LocalTextStyle.current.copy(lineHeight = 15.sp),
-                    fontSize = 14.sp,
-                    maxLines = 2,
-                )
-                Text(
-                    text = duration,
-                    color = MaterialTheme.colorScheme.onBackground,
-                    fontFamily = SpaceGroteskFontFamily,
-                    fontWeight = FontWeight.Thin,
-                    style = LocalTextStyle.current.copy(lineHeight = 15.sp),
-                    fontSize = 14.sp,
-                    maxLines = 2,
-                )
-            }
-        }
-    }
-}
 
 @Composable
 fun RecordingMenuItem(
@@ -137,9 +68,7 @@ fun RecordingMenuItem(
             RecordingMenuItemBase(
                 title = title,
                 date = dateFormatter(date),
-                id = id,
                 duration = if (durationText.isNotBlank()) TimeFormatter().timeFormatter(durationText.toLong()) else "",
-                color = color,
                 isFirstItem = isFirstItem,
             )
         }
