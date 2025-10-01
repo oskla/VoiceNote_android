ca794928fb26193809309771934097607c486fde
Oskar Larsson <90769758+oskla@users.noreply.github.com>
2025-10-01 11:05:39 +0200

    Implement version catalog throughout the app

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -4,23 +4,15 @@ plugins {
 }
 
 android {
-    namespace = "com.larsson.voicenote_android.audioplayer"
+    namespace = "com.larsson.voicenote_android.domain"
     compileSdk = 35
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
 }
 
 dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.media3.exoplayer)
-    implementation(libs.androidx.media3.common)
-    implementation(libs.androidx.media3.session)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core.v351)
